bbc74ff0e1888991a4ccc6eb595e230916163d62
Schneider-sizeof <ayyoubberoigui@gmail.com>
2026-05-14 05:52:46 +0200

    Fix product card height and update site email

--- core/context_processors.py
+++ core/context_processors.py
@@ -9,7 +9,7 @@ def site_settings(request):
         settings = SiteSettings(
             site_name='CAPERSMED',
             phone='+212 6 61 48 28 83',
-            email='info@capersmed.com',
+            email='capersmed.maroc@gmail.com',
             address='Hay Namae Bensouda 371/3, Fes 30000, Morocco'
         )
     return {
